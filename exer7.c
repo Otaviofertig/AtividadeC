50638b40d358a45c03047b12dc0f19babb542d13
Otaviofertig <otavio.lesss@gmail.com>
2026-02-25 14:01:50 -0300

    commit

--- exer7.c
+++ exer7.c
@@ -4,8 +4,8 @@ int main() {
     float a = 5.0, b = 5.0, c = 8.0;
     float media = (a+b+c) / 3;
     printf("Sua media: %.2f\n", media);
-    if (media >= 6.0) printf("Aprovado\n");
-    else printf("Reprovado\n"); 
+    if (media >= 6.0) printf("Você está Aprovado\n");
+    else printf("Você foi Reprovado\n"); 
     return 0;
 }
 
